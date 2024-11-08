98abb2a3b673df36d5dfc5d6b9ae4429e5c27cbc
Baruh Ifraimov <baruh.ifraimov@gmail.com>
2024-11-08 15:47:46 +0200

    Added classes and implemented interfaced for relevant classes.

---
+++ Discs.java
@@ -0,0 +1,16 @@
+public class Discs implements Disc {
+    @Override
+    public Player getOwner() {
+        return null;
+    }
+
+    @Override
+    public void setOwner(Player player) {
+
+    }
+
+    @Override
+    public String getType() {
+        return "";
+    }
+}
---
+++ Move.java
@@ -0,0 +1,16 @@
+public class Move {
+
+    public Move(){
+
+    }
+
+    public Position position(){
+
+        return null;
+    }
+
+    public Disc disc(){
+
+        return null;
+    }
+}
